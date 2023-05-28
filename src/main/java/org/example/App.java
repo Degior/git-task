143cdf67b3ffb3cddbd0fa0160f211676534d9cd
Degior <matsegoralex@gmail.com>
2023-05-28 17:51:28 +0500

    some changes

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -8,7 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Bonjour le monde!" );
         System.out.println( "Привет мир!" );
         System.out.println("مرحبا بالعالم");
     }
