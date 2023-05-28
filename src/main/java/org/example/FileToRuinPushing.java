eaf6fd14d0daf17d41b6d64593c6882f07f1c2ed
mariamarfitcyna <marfitcynamaria@gmail.com>
2023-05-28 18:37:54 +0500

    main branch was changed

---
+++ src/main/java/org/example/FileToRuinPushing.java
@@ -0,0 +1,8 @@
+package org.example;
+
+public class FileToRuinPushing {
+    public static void main( String[] args )
+    {
+        System.out.println( "I will ruin push task2 branches!" );
+    }
+}
